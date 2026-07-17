7b3439ba6b6c6eab47fd3f9cc02bf05b6e79efd6
Aran <76783878+Vikhorz@users.noreply.github.com>
2026-07-17 14:44:32 +0300

    Refactor SessionProvider to include ThemeProvider

--- offside/app/providers.tsx
+++ offside/app/providers.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { SessionProvider as NextSessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 
-export function SessionProvider({ children }: { children: React.ReactNode }) {
-  return <NextSessionProvider>{children}</NextSessionProvider>;
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="data-theme" defaultTheme="dark" enableSystem={false}>
+      <NextSessionProvider>{children}</NextSessionProvider>
+    </ThemeProvider>
+  );
 }
