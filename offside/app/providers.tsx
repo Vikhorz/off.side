542b0826d88c9d20d4713cffd1495f2de7e5f223
Aran <76783878+Vikhorz@users.noreply.github.com>
2026-07-18 18:34:49 +0300

    Wrap NextSessionProvider with LanguageProvider

--- offside/app/providers.tsx
+++ offside/app/providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { SessionProvider as NextSessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
+import { LanguageProvider } from "@/lib/i18n";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="data-theme" defaultTheme="dark" enableSystem={false}>
-      <NextSessionProvider>{children}</NextSessionProvider>
+      <LanguageProvider>
+        <NextSessionProvider>{children}</NextSessionProvider>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }
